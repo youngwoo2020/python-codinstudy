4b4c5809e78749648c722551d12916a72cb7c04e
MinyoungLee1 <102496019+MinyoungLee1@users.noreply.github.com>
2022-04-05 09:25:21 +0900

    Update day3_문자열 내 p y 개수 어쩌고.py

--- minyoung/day3_문자열 내 p y 개수 어쩌고.py
+++ minyoung/day3_문자열 내 p y 개수 어쩌고.py
@@ -1,13 +1,8 @@
 def solution(s):
-    count_p = 0
-    count_y = 0
     s = s.lower()
 
-    for i in range(len(s)):
-        if s[i] == 'p':
-            count_p += 1
-        elif s[i] == 'y':
-            count_y += 1
+    count_p = s.count('p')
+    count_y = s.count('y')
 
     if count_p == count_y:
         return True
