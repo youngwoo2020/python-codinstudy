b7708381aac28a990e2f29a15b4d5ccb99d12a3a
youngwoo2020 <83528601+youngwoo2020@users.noreply.github.com>
2022-04-14 13:20:27 +0900

    0414

---
+++ youngwoo/day8/같은숫자싫어.py
@@ -0,0 +1,8 @@
+arr = [1,1,3,3,0,1,1]
+
+answer=[]
+
+for i in range(len(arr)):
+    answer.append(arr[i])
+        
+print(answer)
---
+++ youngwoo/day9/콜라츠추측.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    while n>1 and answer<500:
+        if n%2==0:
+            n /= 2
+            answer += 1
+        elif n%2==1:
+            n = n*3+1
+            answer +=1
+    if answer>=500:
+        answer = -1
+    return answer1
