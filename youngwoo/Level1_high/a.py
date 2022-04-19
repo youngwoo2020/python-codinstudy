f89c5924b8c68688cbdcb139d94d51eab604a6fd
youngwoo2020 <83528601+youngwoo2020@users.noreply.github.com>
2022-04-19 17:19:09 +0900

    신고한다

--- youngwoo/Level1_high/a.py
+++ youngwoo/Level1_high/a.py
@@ -4,36 +4,47 @@ k=2
 id_list=["muzi", "frodo", "apeach", "neo"]
 report=["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 
-def solution(id_list, report, k):
-    answer = []
-    count = {}
-    report = list(dict.fromkeys(report))
-    for i in report:
-        print(i.split(" ")[1])
-        try: count[i.split(" ")[1]]+=1
-        except : count[i.split(" ")[1]]=1
+
+answer = []
+start=[]
+end=[]
+report = list(dict.fromkeys(report))
+
+for i in report:
+    end.append(i.split(" ")[1])  
+
+
+fail =[]        
+count = {}
+for i in end:
+    try: 
+        count[i] +=1
+        if count[i]>=k:
+            count[i]=k
+            continue
+    except: count[i] = 1
+    if count[i]==k:
+        fail.append(count.keys()[i])
+
+# for i in range(len(list(count.values()))):
+#     if list(count.values())[i]>=k:
+#         fail.append(list(count.keys())[i])
         
+        
+print(fail)
+new_report =[]
+for i in report:
+    new_report.append(i.split(" "))
 
-    fail =[]
-    for i in range(len(list(count.values()))):
-        if list(count.values())[i]>=k:
-            fail.append(list(count.keys())[i])
-            
-    new_report =[]
-    for i in report:
-        new_report.append(i.split(" "))
-
-    pre = []
-    for j in range(len(new_report)):
-        for i in range(len(fail)):
-            if fail[i] == new_report[j][1]:
-                pre.append(new_report[j][0])
+pre = []
+for j in range(len(new_report)):
+    for i in range(len(fail)):
+        if fail[i] == new_report[j][1]:
+            pre.append(new_report[j][0])
 
 
-
-    answer = [0 for i in range(len(id_list))]
-    for i in range(len(id_list)):
-        for j in range(len(pre)):
-            if id_list[i]==pre[j]:
-                answer[i]+=1
-    return answer
+answer = [0 for i in range(len(id_list))]
+for i in range(len(id_list)):
+    for j in range(len(pre)):
+        if id_list[i]==pre[j]:
+            answer[i]+=1
--- youngwoo/Level1_high/신고결과받기.py
+++ youngwoo/Level1_high/신고결과받기.py
@@ -1,42 +1,42 @@
-
-from posixpath import split
-from collections import Counter
-
-k=2
-id_list=["muzi", "frodo", "apeach", "neo"]
-report=["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-
-
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
 answer = []
+
 start=[]
 end=[]
 report = list(dict.fromkeys(report))
 
 for i in report:
     end.append(i.split(" ")[1])  
-
-        
 count = {}
 for i in end:
-    try: count[i] +=1
+    try: 
+        count[i] +=1
+        if count[i]>=k:
+            count[i]=k
+            continue
     except: count[i] = 1
 
 fail =[]
 for i in range(len(list(count.values()))):
     if list(count.values())[i]>=k:
+
         fail.append(list(count.keys())[i])
-        
+
 new_report =[]
 for i in report:
     new_report.append(i.split(" "))
+    
+print(fail)
+
 
 pre = []
+
 for j in range(len(new_report)):
     for i in range(len(fail)):
         if fail[i] == new_report[j][1]:
             pre.append(new_report[j][0])
-print(new_report)        
-print(pre)
 
 
 answer = [0 for i in range(len(id_list))]
@@ -44,5 +44,3 @@ for i in range(len(id_list)):
     for j in range(len(pre)):
         if id_list[i]==pre[j]:
             answer[i]+=1
-
-
